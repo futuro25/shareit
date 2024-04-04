df30745692e666e026eb5a7ba8820432a27426a7
“Leandro” <admin@admin.local>
2024-04-04 20:00:11 -0300

    add brand

--- client/src/components/Share.js
+++ client/src/components/Share.js
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import React, {useState, useRef} from "react";
 import QRCode from 'react-qr-code';
+import { config } from '../config';
 
 export default function Share({phone}) {
   const API = 'https://api.whatsapp.com/send?phone=';
@@ -10,6 +11,7 @@ export default function Share({phone}) {
     <div className="px-4 h-full overflow-auto mt-4">
       <div className="w-full flex flex-col sticky top-0 z-10 bg-white rounded pb-4 items-center justify-center">
         <QRCode value={link} />
+        <p className="text-gray-500 text-sm italic"><a href={config.baseUrl}>share-it.com</a></p>
       </div>
     </div>
   );
